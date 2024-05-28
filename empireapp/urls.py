3cc8535f1f2e07a097ddd83f21314457a5c8880b
Juan Pablo Arr <ju.arriagadar@duocuc.cl>
2024-05-28 10:49:17 -0400

    Paginas agregas y bd

--- empireapp/urls.py
+++ empireapp/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 
-from empireapp.views import index, productos
+from empireapp.views import index, productos, detalle_producto, contacto, blog, about
 
 #URLS DE APLICACION
 urlpatterns = [
     path('', index, name='index'), 
     path('productos/', productos, name="productos"),
+    path('detalle-producto/', detalle_producto, name="detalle-producto"),
+    path('contacto/', contacto, name="contacto"),
+    path('blog/', blog, name="blog"),
+    path('about/', about, name="about"),
 ]
--- empireapp/views.py
+++ empireapp/views.py
@@ -2,7 +2,19 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request,"empireapp/index.html")
+    return render(request, "empireapp/index.html")
 
 def productos(request):
     return render(request, "empireapp/pages/productos.html")
+
+def detalle_producto(request):
+    return render(request, "empireapp/detalle-producto.html")
+
+def contacto(request):
+    return render(request, "empireapp/contact.html")
+
+def blog(request):
+    return render(request, "empireapp/blog.html")
+
+def about(request):
+    return render(request, "empireapp/about.html")
